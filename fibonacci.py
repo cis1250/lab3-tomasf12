a2f98c62e975ae235b7f96537d9e4621da149c61
Tomas Fickert <tomas.fickert@gmail.com>
2025-09-29 00:43:00 +0000

    Completed Lab a) fibonacci sequence

--- fibonacci.py
+++ fibonacci.py
@@ -1,7 +1,25 @@
-#!/usr/bin/env python3
+while True: #infinite loop for error handling
+    user_input = input("Enter number of terms: ") 
+    try: 
+        terms = int(user_input)
+        if terms > 0:
+            break   #end loop if condition is met
+        else:
+            print("Please enter a positive integer.")
+    except:    #handles other exception (x !< 0, x !> 0)
+          print("Please enter a positive integer.")
+fibonacci= []
+for fib in range(terms): #first two terms are given in fibonacci
+    if fib == 0:
+        fibonacci.append(fib)
+    elif fib == 1:
+        fibonacci.append(fib)
+    else:
+        fibonacci.append((fibonacci[-1]) + (fibonacci[-2])) #fibonacci sequence, fn = fn-1 + fn-2
+print(*fibonacci) #* removes brackets and seperates by space
+    
 
-# Fibonacci Sequence Exercise
-# TODO: (Read detailed instructions in the Readme file)
-# Prompt the user for the number of terms.
-# Validate that the input is a positive integer.
-# Use a for loop to print the Fibonacci sequence up to that many terms.
+    
+    
+   
+    
